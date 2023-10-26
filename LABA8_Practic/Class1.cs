9e75ee85e84fc5141892b53ac5daa3f2042e84ec
Alexander Chechetko <ggg.kacheck@gmail.com>
2023-10-26 21:27:36 +0600

    Добавьте файлы проекта.

---
+++ LABA8_Practic/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABA8_Practic
+{
+    class Class1
+    {
+ 
+        static void Main(string[] args)
+        {
+            // Получение текущего времени
+            DateTime currentTime = DateTime.Now;
+
+            // Установка времени для проверки скидки (с 8:00 до 12:00)
+            DateTime discountStartTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 8, 0, 0);
+            DateTime discountEndTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 12, 0, 0);
+
+            // Пример ряда продуктов и их цен
+            string[] products = { "Семга", "Красная игра", "Креветки" };
+            double[] prices = { 2.0, 1.5, 3.0 };
+
+            double total = 0.0;
+
+            Console.WriteLine("Продукты в корзине:");
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine($"{products[i]} - {prices[i]}");
+
+                if (currentTime >= discountStartTime && currentTime <= discountEndTime)
+                {
+                    // Применяем скидку 5% в указанное время
+                    total += prices[i] * 0.95;
+                }
+                else
+                {
+                    total += prices[i];
+                }
+            }
+
+            Console.WriteLine($"Итого: {total}");
+        }
+    }
+}
---
+++ LABA8_Practic/RangeOfArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABA8_Practic
+{
+    public class RangeOfArray
+    {
+        private int lowerBound; // Нижний индекс
+        private int upperBound; // Верхний индекс
+        private int[] array;    // Массив данных
+
+        public RangeOfArray(int lower, int upper)
+        {
+            lowerBound = lower;  // Инициализируем нижний индекс пользовательским значением.
+            upperBound = upper;  // Инициализируем верхний индекс пользовательским значением.
+            array = new int[upper - lower + 1];  // Создаем массив, размер которого зависит от пользовательских индексов.
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (IsIndexValid(index)) // Проверяем, допустим ли данный индекс.
+                {
+                    return array[index - lowerBound];  // Возвращаем элемент массива с учетом смещения на нижний индекс.
+                }
+                else
+                {
+                    Console.WriteLine("Недопустимый индекс.");  // Если индекс недопустим, выводим сообщение и возвращаем 0.
+                    return 0;
+                }
+            }
+            set
+            {
+                if (IsIndexValid(index))  // Проверяем, допустим ли данный индекс.
+                {
+                    array[index - lowerBound] = value;  // Устанавливаем значение элемента массива с учетом смещения на нижний индекс.
+                }
+                else
+                {
+                    Console.WriteLine("Недопустимый индекс.");  // Если индекс недопустим, выводим сообщение.
+                }
+            }
+        }
+
+        private bool IsIndexValid(int index)
+        {
+            return index >= lowerBound && index <= upperBound;  // Проверяем, находится ли индекс в допустимом диапазоне.
+        }
+    }
+}
